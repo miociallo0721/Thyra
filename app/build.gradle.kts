2648374fc80d6b7c1edd1795dd104c2085551665
Mio <miociallo0721@gmail.com>
2026-09-25 20:11:56 +0800

    chore: bump Android app version to 1.0.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -34,8 +34,8 @@ android {
         applicationId = "dev.thyra.android"
         minSdk = 26
         targetSdk = 36
-        versionCode = 3
-        versionName = "1.0.2"
+        versionCode = 4
+        versionName = "1.0.3"
     }
 
     signingConfigs {
